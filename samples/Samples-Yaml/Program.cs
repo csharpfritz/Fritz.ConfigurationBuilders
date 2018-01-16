e0ac7e98083a3e6c544210cfe21f99ed79d99e10
Jeff Fritz <jeffreytfritz@gmail.com>
2018-01-16 12:07:35 -0500

    Added YAML sample

---
+++ samples/Samples-Yaml/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples_Yaml
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var keys = ConfigurationManager.AppSettings.AllKeys;
+
+			Console.WriteLine($"Here are my current {keys.Length}: AppSettings");
+
+			foreach (var item in keys)
+			{
+
+				Console.WriteLine($"{item}: {ConfigurationManager.AppSettings[item]}");
+
+			}
+
+			Console.ReadLine();
+
+
+		}
+	}
+}
