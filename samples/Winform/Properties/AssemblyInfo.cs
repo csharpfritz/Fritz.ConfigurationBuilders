598b178e3d3ee04305a7d60ccb9c869d9e70c32c
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2018-02-04 16:10:06 -0500

    Completed initial RSS and YAML configuration builders (#12)
    * Can now read merged section names for INI files
    
    * Added rss configuration builder + sample console
    
    * Added RSS web endpoint + minor change in settings reading
    
    * Updated nuspec
    
    * YAML v1 (#10)
    
    First implementation of a YAML configuration builder
    
    * Tagged 0.4-preview
    
    * Added WinForm with ApplicationSettings sample
    
    * Updated files

--- samples/Winform/Properties/AssemblyInfo.cs
+++ samples/Winform/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fritz.ConfigurationBuilders")]
+[assembly: AssemblyTitle("WinForm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fritz.ConfigurationBuilders")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("WinForm")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8d521abc-f019-4b36-99a8-3e2c431a5f6e")]
+[assembly: Guid("2144f1ce-3a7c-448d-b153-e273a6afd5e9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0")]
-[assembly: AssemblyFileVersion("0.4.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
